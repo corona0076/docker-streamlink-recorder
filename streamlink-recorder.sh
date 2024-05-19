0dd809516d545ef1b70021aae538b2ca77a35d3e
corona0076 <108451266+corona0076@users.noreply.github.com>
2024-05-19 23:29:34 +0900

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -11,7 +11,6 @@ IFS=';' read -r -a args <<< "$streamOptions"
 
 while [ true ]; do
 	Date=$(date +'%Y-%m-%d %Hh-%Mm-%Ss')
- 	output_file="/home/download/$streamName/${streamName}_t - $Date.ts"
-	streamlink "${args[@]}" "$streamLink" "$streamQuality" -o "$output_file"
+	streamlink "${args[@]}" "$streamLink" "$streamQuality" -o /home/download/"$streamName"/"$streamName"-"$Date".ts
 	sleep 10s
 done
